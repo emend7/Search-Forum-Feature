8ee09c806c71f1bf56b3a2117eb6da7c2ccf53c2
emend7 <emend7@uic.edu>
2024-04-09 01:41:07 -0500

    rough product

--- main.py
+++ main.py
@@ -8,32 +8,92 @@ import sqlite3
 import objecttier
 
 # have a few options on how many items you want to search for
-
+dbconn = sqlite3.connect('Forum.db')
 # usecase 1: want to search by product id tag
+def returnStats(dbconn):
+    posts = objecttier.num_posts(dbconn)
+    comments = objecttier.num_comments(dbconn)
+    products = objecttier.num_products(dbconn)
+    print(f"""General Statistics:
+   Number of Posts: {posts:,}
+   Number of Comments: {comments:,}
+   Number of Products: {products:,}""")
+
+def searchbyID(dbconn):
+    product_id = input("What is the product id you are looking for? ")
+    results = objecttier.filter_by_PID(dbconn, product_id)
+    for row in results:
+        print(f"""----------
+Title: {row.Title}
+Description: {row.Description}
+Solution: {row.Solution}
+TSR Approved: {row.Approved}
+Industry: {row.Industry}
+----------
+""")
+
 # usecase 2: want to search by product name
+def searchbyName(dbconn):
+    name = input("What is the name of the product you are looking for? ")
+    results = objecttier.filter_by_Name(dbconn, name)
+    for row in results:
+        print(f"""----------
+Title: {row.Title}
+Description: {row.Description}
+Solution: {row.Solution}
+TSR Approved: {row.Approved}
+Industry: {row.Industry}
+----------
+""")
+
 # usecase 3: want to search by industry
+def searchbyIndustry(dbconn):
+    industry = input("What industry are you looking for? ")
+    results = objecttier.filter_by_industry(dbconn, industry)
+    for row in results:
+        print(f"""----------
+Title: {row.Title}
+Description: {row.Description}
+Solution: {row.Solution}
+TSR Approved: {row.Approved}
+Industry: {row.Industry}
+----------
+""")
+
 # usecase 4: want to search by problem
+def searchbyProblem(dbconn):
+    problem = input("What is your problem? ")
+    results = objecttier.filter_by_problem(dbconn, problem)
+    for row in results:
+        print(f"""----------
+Title: {row.Title}
+Description: {row.Description}
+Solution: {row.Solution}
+TSR Approved: {row.Approved}
+Industry: {row.Industry}
+----------
+""")
 
 # Press the green button in the gutter to run the script.
-print("** Welcome to the Search Forum Database Application")
-print('** Welcome to the Chicago Lobbyist Database Application **\n')
+print("** Welcome to the Search Forum Database Application **\n")
 print()
+returnStats(dbconn)
 
 choice = input("Please enter a command (1-4, x to exit): ")
 
 while choice != "x":
     if choice == "1":
         # usecase 1: want to search by product id tag
-        pass
+        searchbyID(dbconn)
     elif choice == "2":
         # usecase 2: want to search by product name
-        pass
+        searchbyName(dbconn)
     elif choice == "3":
         # usecase 3: want to search by industry
-        pass
+        searchbyIndustry(dbconn)
     elif choice == "4":
         # usecase 4: want to search by problem
-        pass
+        searchbyProblem(dbconn)
     else:
         print("**Error, unknown command, try again...\n")
 
--- objecttier.py
+++ objecttier.py
@@ -1,9 +1,9 @@
-
 import datatier
-class Post:
 
-    def __init__(self, id, title, desc, time, up, down, approved, industry, resolved):
-        self._Review_ID = id
+
+class Post:
+    def __init__(self, objectID, pid, title, desc, time, up, down, approved, industry, resolved, solution):
+        self._Review_ID = objectID
         self._Title = title
         self._Description = desc
         self._Read_Time = time
@@ -12,6 +12,8 @@ class Post:
         self._Approved = approved
         self._Industry = industry
         self._Resolved = resolved
+        self._Solution = solution
+        self._Product_ID = pid
 
     @property
     def Review_ID(self):
@@ -25,20 +27,184 @@ class Post:
     def Description(self):
         return self._Description
 
+    @property
+    def Read_Time(self):
+        return self.Read_Time()
+
+    @property
+    def Upvote(self):
+        return self._Upvote
+
+    @property
+    def Downvote(self):
+        return self._Downvote
+
+    @property
+    def Approved(self):
+        return self._Approved
+
+    @property
+    def Industry(self):
+        return self._Industry
+
+    @property
+    def Resolved(self):
+        return self._Resolved
+
+    @property
+    def Solution(self):
+        return self._Solution
+
+    @property
+    def Product_ID(self):
+        return self._Product_ID
 
 
 class Comments:
-    def __init__(self, id):
+    def __init__(self, id, qid, body, uid, upvote, downvote):
         self._Comments_ID = id
+        self._Question_ID = qid
+        self._Body = body
+        self._User_ID = uid
+        self._Upvote = upvote
+        self._Downvote = downvote
 
     @property
     def Comments_ID(self):
         return self._Comments_ID
 
+    @property
+    def Question_ID(self):
+        return self._Question_ID
+
+    @property
+    def Body(self):
+        return self._Body
+
+    @property
+    def User_ID(self):
+        return self._User_ID
+
+    @property
+    def Upvote(self):
+        return self._Upvote
+
+    @property
+    def Downvote(self):
+        return self._Downvote
+
+
 class Product:
-    def __init__(self, id):
+    def __init__(self, id, name, link):
         self._Product_ID = id
+        self._Name = name
+        self._Link = link
 
     @property
     def Comments_ID(self):
         return self._Product_ID
+
+    @property
+    def Name(self):
+        return self._Name
+
+    @property
+    def Link(self):
+        return self._Link
+
+
+# Returns: number of posts in the database
+#           if an error occurs, the function returns -1
+def num_posts(dbConn):
+    try:
+        sql = "SELECT DISTINCT COUNT(*) FROM Posts"
+        results = datatier.select_one_row(dbConn, sql)
+        return results[0]
+    except Exception as err:
+        return -1
+
+
+# Returns: number of comments in the database
+#           if an error occurs, the function returns -1
+def num_comments(dbConn):
+    try:
+        sql = "SELECT DISTINCT COUNT(*) FROM Comments"
+        results = datatier.select_one_row(dbConn, sql)
+        return results[0]
+    except Exception as err:
+        return -1
+
+
+# Returns: number of products in the database
+#           if an error occurs, the function returns -1
+def num_products(dbConn):
+    try:
+        sql = "SELECT DISTINCT COUNT(*) FROM Products"
+        results = datatier.select_one_row(dbConn, sql)
+        return results[0]
+    except Exception as err:
+        return -1
+
+
+# Usecase 1: want to search by product id tag
+def filter_by_PID(dbConn, product_id):
+    try:
+        sql = "SELECT * FROM Posts WHERE Product_ID = ? ORDER BY Num_Upvotes DESC"
+        results = datatier.select_n_rows(dbConn, sql, [product_id])
+        posts = []
+        for row in results:
+            posts.append(Post(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]))
+        return posts
+    except Exception as err:
+        return -1
+
+
+# Usecase 2: want to search by product name
+def filter_by_Name(dbConn, name):
+    try:
+        # TODO: change to be more loose, if we have more words then we trim down the results
+        like_name = ("""
+        SELECT Review_ID, Posts.Product_ID, Title, Description, Read_Time, Num_Upvotes, Num_Downvotes, Approved, Industry, Is_Resolved, Solution
+FROM Posts
+JOIN Products ON Posts.Product_ID = Products.Product_ID
+WHERE NAME LIKE ?
+ORDER BY Review_ID ASC
+                     """)
+        results = datatier.select_n_rows(dbConn, like_name, [name])
+        posts = []
+        for row in results:
+            posts.append(Post(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]))
+        return posts
+    except Exception as err:
+        return -1
+
+
+# usecase 3: want to search by industry
+def filter_by_industry(dbConn, industry):
+    try:
+        like_industry = """SELECT * FROM Posts WHERE Industry LIKE ? ORDER BY Num_Upvotes DESC"""
+        results = datatier.select_n_rows(dbConn, like_industry, [industry])
+        posts = []
+        for row in results:
+            posts.append(Post(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]))
+        return posts
+    except Exception as err:
+        return -1
+
+
+# usecase 4: want to search by problem
+def filter_by_problem(dbConn, problem):
+    try:
+        like_problem = """
+        SELECT * FROM Posts 
+        WHERE Title LIKE ? OR DESCRIPTION LIKE ? 
+        ORDER BY Num_Upvotes DESC
+        """
+        results = datatier.select_n_rows(dbConn, like_problem, [problem, problem])
+        posts = []
+        for row in results:
+            posts.append(Post(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]))
+        return posts
+
+    except Exception as err:
+        return -1
